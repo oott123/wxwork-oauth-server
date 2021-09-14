680a7d4780e5797d3b557258668932fa67271dab
oott123 <git@public.oott123.com>
2021-09-14 19:54:12 +0800

    chore: remove enc jwks

--- packages/wxwork-oauth-server/src/tools/jwk.ts
+++ packages/wxwork-oauth-server/src/tools/jwk.ts
@@ -7,9 +7,9 @@ const keystore = new KeyStore()
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 Promise.all([
   keystore.generate('RSA', 2048, { use: 'sig' }),
-  keystore.generate('RSA', 2048, { use: 'enc' }),
+  // keystore.generate('RSA', 2048, { use: 'enc' }),
   keystore.generate('EC', 'P-256', { use: 'sig' }),
-  keystore.generate('EC', 'P-256', { use: 'enc' }),
+  // keystore.generate('EC', 'P-256', { use: 'enc' }),
   keystore.generate('OKP', 'Ed25519', { use: 'sig' }),
 ]).then(function() {
   process.stdout.write(JSON.stringify(keystore.toJWKS(true), null, 2))
